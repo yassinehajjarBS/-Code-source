271be3715d57727edd76c218645023e209fafba9
yassinehajjarBS <yassinehajjar66@gmail.com>
2025-09-22 11:51:10 +0200

    Add files via upload

--- CODE SOURCE/code sou 00/script.js
+++ CODE SOURCE/code sou 00/script.js
@@ -1,9 +1,10 @@
 const sos = ["C1", "C3", "C2", "C1", "C8", "C1", "C3", "C2"];
 
-const codeRecherche = prompt("Entrez le code de compétence à compter (ex: C1, C2, ...)");
+const dd = prompt("Entrez le code de compétence à compter (ex: C1, C2, ...)");
 
-if (codeRecherche) {
-  const rar = codeRecherche.trim().toUpperCase();
+if (dd)
+ {
+  const rar = dd.trim().toUpperCase();
   const occurrences = sos.filter(code => code === rar).length;
   console.log(`Le code "${rar}" apparaît ${occurrences} fois dans le tableau.`);
 } else {

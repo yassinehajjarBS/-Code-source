2ed973ea213297f8fa4db8425c01dfa585c6b2c5
yassinehajjarBS <yassinehajjar66@gmail.com>
2025-09-26 09:22:28 +0100

    Delete 11 - Prototype/LIVE CODING directory

--- 11 - Prototype/LIVE CODING/script.js
+++
@@ -1,9 +0,0 @@
-let code00 = ["C1", "C4", "C2", "C1", "C8", "C1", "C3", "C2"];
-let code01 = prompt("Quel code de compétence voulez-vous compter ?");
-let compteur = 0;
-for (let code of code00) {
-  if (code === code01) {
-    compteur++;
-  }
-}
-console.log(`Le code "${code01}" apparaît ${compteur} fois.`);
